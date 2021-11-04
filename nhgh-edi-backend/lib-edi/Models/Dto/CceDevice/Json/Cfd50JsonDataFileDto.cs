d7ec792e0067a17c4e1d1e1c479e157fe007ab4b
Erik Olsen <eriko@valencelevel.com>
2021-11-04 10:17:11 -0700

    nhgh-1770 add latest cfd50 telemetry file spec into pipeline

--- nhgh-edi-backend/lib-edi/Models/Dto/CceDevice/Json/Cfd50JsonDataFileDto.cs
+++ nhgh-edi-backend/lib-edi/Models/Dto/CceDevice/Json/Cfd50JsonDataFileDto.cs
@@ -6,39 +6,37 @@ namespace lib_edi.Models.Dto.Loggers
 {
 	public class Cfd50JsonDataFileDto
 	{
-		#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-		public string AMFR { get; set; }
-		public string AMOD { get; set; }
-		public string APQS { get; set; }
-		public string ASER { get; set; }
-		public string AID { get; set; }
-		public string ADAT { get; set; }
-		public string CID { get; set; }
-		public string FID { get; set; }
-		public string LOC { get; set; }
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+		public string AMFR { get; set; } //1104
+		public string AMOD { get; set; } //1104
+		public string ASER { get; set; } //1104
+		public string ADOP { get; set; } //1104
+		public string APQS { get; set; } //1104
+		public string RNAM { get; set; } //1104
+		public string DNAM { get; set; } //1104
+		public string FNAM { get; set; } //1104
+		public string CID { get; set; } //1104
+		public string LAT { get; set; } //1104
+		public string LNG { get; set; } //1104
+
 		public IList<Cfd50JsonDataFileRecordDto> records { get; set; }
-		#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 	}
 
 	public class Cfd50JsonDataFileRecordDto
 	{
-		#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-		public string ABST { get; set; }
-		public string TAMB { get; set; }
-		public string TCLD { get; set; }
-		public string TVC { get; set; }
-		public string CMPR { get; set; }
-		public string SVA { get; set; }
-		public string EVDC { get; set; }
-		public string CDRW { get; set; }
-		public string DOOR { get; set; }
-		public string HOLD { get; set; }
-		public string BEMD { get; set; }
-		public string TCON { get; set; }
-		public string CMPS { get; set; }
-		public string CSOF { get; set; }
-		public string[] alerts { get; set; }
-		public string[] errors { get; set; }
-		#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+		public string ABST { get; set; } //1104
+		public string SVA { get; set; } //1104
+		public string HAMB { get; set; } //1104
+		public string TAMB { get; set; } //1104
+		public string ACCD { get; set; } //1104
+		public string TCON { get; set; } //1104
+		public string TVC { get; set; } //1104
+		public string BEMD { get; set; } //1104
+		public string HOLD { get; set; } //1104
+		public string DORV { get; set; } //1104
+		public string ALRM { get; set; } //1104
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 	}
 }
