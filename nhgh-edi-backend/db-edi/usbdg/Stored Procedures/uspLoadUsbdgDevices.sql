c36629580459ef19ac20b34c17775a5403366b7a
Erik Olsen <eriko@valencelevel.com>
2022-07-05 09:21:02 -0700

    nhgh-2410 add upsert to usbdg device sql table

--- nhgh-edi-backend/db-edi/usbdg/Stored Procedures/uspLoadUsbdgDevices.sql
+++ nhgh-edi-backend/db-edi/usbdg/Stored Procedures/uspLoadUsbdgDevices.sql
@@ -1,4 +1,5 @@
 ﻿
+
 CREATE PROCEDURE [usbdg].[uspLoadUsbdgDevices] @usbdg_device [usbdg].[device_type] READONLY
 AS
 BEGIN
@@ -6,15 +7,26 @@ BEGIN
 	USING @usbdg_device AS s
 	ON t.[ESER] = s.[ESER]
 	WHEN NOT MATCHED THEN 
-	INSERT ([EDOP],[EMFR],[EMOD],[EMSV],[EPQS],[ESER],[zcfg_ver],[zmcu_ver],[DATEADDED]) 
-	VALUES(
-	s.[EDOP],
-	s.[EMFR],
-	s.[EMOD],
-	s.[EMSV],
-	s.[EPQS],
-	s.[ESER],
-	s.[zcfg_ver],
-	s.[zmcu_ver],
-	GETDATE());
+		INSERT ([EDOP],[EMFR],[EMOD],[EMSV],[EPQS],[ESER],[zcfg_ver],[zmcu_ver],[DATEADDED]) 
+		VALUES(
+		s.[EDOP],
+		s.[EMFR],
+		s.[EMOD],
+		s.[EMSV],
+		s.[EPQS],
+		s.[ESER],
+		s.[zcfg_ver],
+		s.[zmcu_ver],
+		GETDATE())
+	WHEN MATCHED THEN UPDATE SET
+		t.[EDOP] = s.[EDOP],
+		t.[EMFR] = s.[EMFR],
+		t.[EMOD] = s.[EMOD],
+		t.[EMSV] = s.[EMSV],
+		t.[EPQS] = s.[EPQS],
+		t.[ESER] = s.[ESER],
+		t.[zcfg_ver] = s.[zcfg_ver],
+		t.[zmcu_ver] = s.[zmcu_ver],
+		t.[LASTMODIFIED] = GETDATE()
+	;
 END
