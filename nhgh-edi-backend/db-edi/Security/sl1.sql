07ada91caa609083e0393a0cc69855e8aa747bb5
Erik Olsen <eriko@valencelevel.com>
2022-10-13 09:10:10 -0700

    nhgh-2598 add sl1 database objects

---
+++ nhgh-edi-backend/db-edi/Security/sl1.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [sl1]
+    AUTHORIZATION [dbo];
+
---
+++ nhgh-edi-backend/db-edi/sl1/Stored Procedures/uspLoadSl1Events.sql
@@ -0,0 +1,46 @@
+﻿
+
+CREATE PROCEDURE [sl1].[uspLoadSl1Events] @sl1_event [sl1].[event_type] READONLY
+AS
+BEGIN
+	MERGE [sl1].[event] AS t
+	USING @sl1_event AS s
+	ON t.[RELT] = s.[RELT] and t.[LSER] = s.[LSER]
+	WHEN NOT MATCHED THEN 
+	INSERT ([ABST_CALC],[ADOP],[ALRM],[AMOD],[AMFR],[APQS],[ASER],[BLOG],[ESER],[LDOP],[LERR],[LMFR],[LMOD],[LPQS],[LSER],[LSV],[RELT],[RTCW],[TAMB],[TVC],[ZCHRG],[ZSTATE],[ZVLVD],[CNAM],[CSER],[CSOF],[CMPR],[CMPS],[DCCD],[DCSV],[FANS],[SVA],[_RELT_SECS],[DATEADDED]) 
+	VALUES(
+	s.[ABST_CALC],
+	s.[ADOP],
+	s.[ALRM],
+	s.[AMOD],
+	s.[AMFR],
+	s.[APQS],
+	s.[ASER],
+	s.[BLOG],
+	s.[ESER],
+	s.[LDOP],
+	s.[LERR],
+	s.[LMFR],
+	s.[LMOD],
+	s.[LPQS],
+	s.[LSER],
+	s.[LSV],
+	s.[RELT],
+	s.[RTCW],
+	s.[TAMB],
+	s.[TVC],
+	s.[ZCHRG],
+	s.[ZSTATE],
+	s.[ZVLVD],
+    s.[CNAM],
+    s.[CSER],
+    s.[CSOF],
+    s.[CMPR],
+    s.[CMPS],
+    s.[DCCD],
+    s.[DCSV],
+    s.[FANS],
+    s.[SVA],
+	s.[_RELT_SECS],
+	GETDATE());
+END
