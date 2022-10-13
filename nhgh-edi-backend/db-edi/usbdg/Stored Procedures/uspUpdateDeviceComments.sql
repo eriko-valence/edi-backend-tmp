1fe4289208b875f982796d23327a7596c457c5f2
Erik Olsen <eriko@valencelevel.com>
2022-10-13 09:07:44 -0700

    nhgh-2599 add editable usbdg device comments field

---
+++ nhgh-edi-backend/db-edi/usbdg/Stored Procedures/uspUpdateDeviceComments.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [usbdg].[uspUpdateDeviceComments]
+  @eser varchar(50), @comments varchar(1000), @result INT OUTPUT
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			IF EXISTS(SELECT 1 FROM [usbdg].[device] WHERE ESER = @eser)
+                BEGIN
+                    UPDATE [usbdg].[device]
+                    SET COMMENTS = @comments
+                    WHERE
+                    ESER = @eser
+                    IF @@rowcount = 1
+                        SET @result = 1 --successful update
+                    ELSE
+                        SET @result = 4 --unsuccessful update 
+                END
+                ELSE
+                    SET @result = 5 --device not found
+		COMMIT
+	END TRY
+	BEGIN CATCH
+		SET @result = 6 --unsuccessful update
+		ROLLBACK
+	END CATCH	
+END
