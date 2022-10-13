d85de3fc931d15bd5b944e32a2a36c96409fefd3
Erik Olsen <eriko@valencelevel.com>
2022-10-13 09:03:20 -0700

    nhgh-2599 add editable usbdg device comments field

--- nhgh-edi-backend/db-edi/usbdg/Tables/device.sql
+++ nhgh-edi-backend/db-edi/usbdg/Tables/device.sql
@@ -1,16 +1,19 @@
 ﻿CREATE TABLE [usbdg].[device] (
-    [EDOP]         DATE          NULL,
-    [EMFR]         VARCHAR (100) NULL,
-    [EMOD]         VARCHAR (100) NULL,
-    [EMSV]         VARCHAR (20)  NULL,
-    [EPQS]         VARCHAR (20)  NULL,
-    [ESER]         VARCHAR (50)  NOT NULL,
-    [zcfg_ver]     VARCHAR (20)  NULL,
-    [zmcu_ver]     VARCHAR (20)  NULL,
-    [DATEADDED]    DATETIME2 (7) NULL,
-    [LASTMODIFIED] DATETIME2 (7) NULL,
-    PRIMARY KEY CLUSTERED ([ESER] ASC)
+    [EDOP]         DATE           NULL,
+    [EMFR]         VARCHAR (100)  NULL,
+    [EMOD]         VARCHAR (100)  NULL,
+    [EMSV]         VARCHAR (20)   NULL,
+    [EPQS]         VARCHAR (20)   NULL,
+    [ESER]         VARCHAR (50)   NOT NULL,
+    [zcfg_ver]     VARCHAR (20)   NULL,
+    [zmcu_ver]     VARCHAR (20)   NULL,
+    [DATEADDED]    DATETIME2 (7)  NULL,
+    [LASTMODIFIED] DATETIME2 (7)  NULL,
+    [COMMENTS]     VARCHAR (1000) NULL,
+    CONSTRAINT [PK_usbdg_device] PRIMARY KEY CLUSTERED ([ESER] ASC)
 );
+
+
 
 
 
