63706970d3b465088b60f4d89c883e431b763f46
Erik Olsen <eriko@valencelevel.com>
2022-10-20 19:56:30 -0700

    nhgh-2630 add logger type to edi monitoring page

--- nhgh-edi-backend/db-edi/telemetry/Stored Procedures/getFailedEdiFilePackages.sql
+++ nhgh-edi-backend/db-edi/telemetry/Stored Procedures/getFailedEdiFilePackages.sql
@@ -4,11 +4,31 @@ CREATE PROCEDURE [telemetry].[getFailedEdiFilePackages]
   @EndDate [datetime2](7)
 AS
 BEGIN
-  SELECT * 
-  FROM 
-    [telemetry].[EdiJobStatus] 
-  WHERE 
-    BlobTimeStart > @StartDate AND
-    BlobTimeStart < @EndDate AND
-    DurationSecs IS NULL
+
+	WITH 
+    FilePackageLoggerTypeCTE
+	AS
+	(
+SELECT 
+    FilePackageName, 
+    DataLoggerType
+FROM 
+    [telemetry].[EdiPipelineEvents] 
+WHERE 
+    PipelineStage = 'ADF_TRANSFORM' AND 
+    DataLoggerType IS NOT NULL
+GROUP BY 
+    FilePackageName, DataLoggerType
+	)
+
+
+SELECT t1.*, t2.DataLoggerType
+FROM 
+    [telemetry].[EdiJobStatus] t1
+LEFT OUTER JOIN 
+    FilePackageLoggerTypeCTE t2 ON t2.FilePackageName = t1.FilePackageName
+WHERE 
+    t1.BlobTimeStart > @StartDate AND
+    t1.BlobTimeStart < @EndDate AND
+    t1.DurationSecs IS NULL
 END
