c10394e1214391b2725226c1ece3db81c2dc6136
Erik Olsen <eriko@valencelevel.com>
2022-10-21 16:25:17 -0700

    nhgh-2631 add datagrabber last data column

--- nhgh-edi-backend/db-dashboard/indigo_v2/Stored Procedures/getLoggers.sql
+++ nhgh-edi-backend/db-dashboard/indigo_v2/Stored Procedures/getLoggers.sql
@@ -15,14 +15,32 @@ BEGIN
             GROUP BY
                 [ESER],
                 [LSER]
+        ),
+        DATAGRABBERS_CTE AS
+        (
+            select 
+            a.eser as Id,
+            a.LASTMODIFIED as LastData
+            from [usbdg].[device] a
+        ),
+        INDIGO_LATEST_DG_CTE AS
+        (
+            SELECT * FROM (SELECT *, ROW_NUMBER() OVER (
+                                PARTITION BY [LSER] 
+                                ORDER BY [ABST_CALC] DESC
+                        ) AS [ROW_NUMBER]
+            FROM 
+            INDIGO_LOGGERS_CTE) LOGGERS
+            WHERE 
+            ROW_NUMBER = 1
         )
 
-    SELECT * FROM (SELECT *, ROW_NUMBER() OVER (
-                        PARTITION BY [LSER] 
-                        ORDER BY [ABST_CALC] DESC
-                ) AS [ROW_NUMBER]
-    FROM 
-    INDIGO_LOGGERS_CTE) LOGGERS
-    WHERE 
-    ROW_NUMBER = 1
+        SELECT 
+            t1.*, t2.LastData as 'LastUsbdgData'
+        FROM 
+            INDIGO_LATEST_DG_CTE t1
+        LEFT OUTER JOIN
+            DATAGRABBERS_CTE t2 ON t2.Id = t1.ESER
+
+
 END
