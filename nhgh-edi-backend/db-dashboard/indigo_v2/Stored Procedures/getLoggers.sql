660392c40febd1c795c62ede726daab3753d7fd3
Erik Olsen <eriko@valencelevel.com>
2022-10-21 15:49:45 -0700

    nhgh-2631 update indigo logger list report to better handle latest connected datagrabbers

--- nhgh-edi-backend/db-dashboard/indigo_v2/Stored Procedures/getLoggers.sql
+++ nhgh-edi-backend/db-dashboard/indigo_v2/Stored Procedures/getLoggers.sql
@@ -2,15 +2,27 @@
 
 AS
 BEGIN
-    SET NOCOUNT ON
-    SELECT
-        MAX([ABST_CALC]) as 'ABST_CALC',
-        [ESER],
-        substring([ESER],9,4) as ShortId,
-        [LSER]
+    WITH
+        INDIGO_LOGGERS_CTE AS
+        (
+            SELECT
+                MAX([ABST_CALC]) AS 'ABST_CALC',
+                [ESER],
+                substring([ESER],9,4) AS ShortId,
+                [LSER]
+            FROM 
+                [indigo_v2].[event]
+            GROUP BY
+                [ESER],
+                [LSER]
+        )
+
+    SELECT * FROM (SELECT *, ROW_NUMBER() OVER (
+                        PARTITION BY [LSER] 
+                        ORDER BY [ABST_CALC] DESC
+                ) AS [ROW_NUMBER]
     FROM 
-        [indigo_v2].[event]
-    GROUP BY
-        [ESER],
-        [LSER]
+    INDIGO_LOGGERS_CTE) LOGGERS
+    WHERE 
+    ROW_NUMBER = 1
 END
