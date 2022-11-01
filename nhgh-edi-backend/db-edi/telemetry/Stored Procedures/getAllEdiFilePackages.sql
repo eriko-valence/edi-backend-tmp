f10ad2a997d0831984fc06b10c099b36d75fae6d
Erik Olsen <eriko@valencelevel.com>
2022-11-01 12:35:46 -0700

    nhgh-2654 add recently processed edi package status results grid to data grabber details page

---
+++ nhgh-edi-backend/db-edi/telemetry/Stored Procedures/getAllEdiFilePackages.sql
@@ -0,0 +1,38 @@
+﻿
+CREATE PROCEDURE [telemetry].[getAllEdiFilePackages]
+  @ESER varchar(64),
+  @StartDate [datetime2](7),
+  @EndDate [datetime2](7)
+AS
+BEGIN
+
+	WITH 
+    FilePackageLoggerTypeCTE
+	AS
+	(
+SELECT 
+    FilePackageName, 
+    DataLoggerType
+FROM 
+    [telemetry].[EdiPipelineEvents] 
+WHERE 
+    PipelineStage = 'ADF_TRANSFORM' AND 
+    DataLoggerType IS NOT NULL AND
+    ESER = @ESER
+GROUP BY 
+    FilePackageName, DataLoggerType
+	)
+
+
+SELECT t1.*, t2.DataLoggerType
+FROM 
+    [telemetry].[EdiJobStatus] t1
+LEFT OUTER JOIN 
+    FilePackageLoggerTypeCTE t2 ON t2.FilePackageName = t1.FilePackageName
+WHERE 
+    t1.BlobTimeStart > @StartDate AND
+    t1.BlobTimeStart < @EndDate AND
+    t1.ESER = @ESER
+ORDER BY 
+    t1.BlobTimeStart DESC
+END
