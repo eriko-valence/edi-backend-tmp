178cdb04bf3e207d4fc31bddce073184e18596eb
Erik Olsen <eriko@valencelevel.com>
2023-08-18 08:49:29 -0700

    nhgh-3057 add emd type to app insights logging

--- fa-adf-transform-varo/Transform.cs
+++ fa-adf-transform-varo/Transform.cs
@@ -99,7 +99,7 @@ namespace fa_adf_transform_varo
 						log.LogInformation($"- {payload.FileName} - Send transformation response");
                         string blobPathFolderCurated = DataTransformService.BuildCuratedBlobFolderPath(payload.Path, verfiedLoggerType);
                         string responseBody = DataTransformService.SerializeHttpResponseBody(blobPathFolderCurated);
-                        DataTransformService.LogEmsTransformSucceededEventToAppInsights(payload.FileName, ediJob.Logger.Type, PipelineStageEnum.Name.ADF_TRANSFORM_VARO, log);
+                        DataTransformService.LogEmsTransformSucceededEventToAppInsights(payload.FileName, ediJob.Emd.Type, ediJob.Logger.Type, PipelineStageEnum.Name.ADF_TRANSFORM_VARO, log);
                         log.LogInformation($"- {payload.FileName} - Done");
 
                         log.LogInformation($" PROCESSING SUMMARY");
@@ -115,7 +115,7 @@ namespace fa_adf_transform_varo
                         loggerTypeEnum = DataLoggerTypeEnum.Name.UNKNOWN;
                         string errorCode = "EHN9";
                         string errorMessage = EdiErrorsService.BuildExceptionMessageString(null, errorCode, EdiErrorsService.BuildErrorVariableArrayList(ediJob.Logger.LMOD));
-                        DataTransformService.LogEmsTransformErrorEventToAppInsights(payload?.FileName, PipelineStageEnum.Name.ADF_TRANSFORM_VARO, log, null, errorCode, errorMessage, loggerTypeEnum, PipelineFailureReasonEnum.Name.UNSUPPORTED_EMS_DEVICE);
+                        DataTransformService.LogEmsTransformErrorEventToAppInsights(payload?.FileName, emdTypeEnum, PipelineStageEnum.Name.ADF_TRANSFORM_VARO, log, null, errorCode, errorMessage, loggerTypeEnum, PipelineFailureReasonEnum.Name.UNSUPPORTED_EMS_DEVICE);
                         //string errorMessage = $"Unknown file package";
                         log.LogError($"- {payload.FileName} - {errorMessage}");
                         var result = new ObjectResult(new { statusCode = 500, currentDate = DateTime.Now, message = errorMessage });
@@ -128,7 +128,7 @@ namespace fa_adf_transform_varo
                 {
                     string errorCode = "KHRD";
                     string errorMessage = EdiErrorsService.BuildExceptionMessageString(null, errorCode, EdiErrorsService.BuildErrorVariableArrayList(payload.FileName));
-                    DataTransformService.LogEmsTransformErrorEventToAppInsights(payload?.FileName, PipelineStageEnum.Name.ADF_TRANSFORM_VARO, log, null, errorCode, errorMessage, loggerTypeEnum, PipelineFailureReasonEnum.Name.UNKNOWN_FILE_PACKAGE);
+                    DataTransformService.LogEmsTransformErrorEventToAppInsights(payload?.FileName, emdTypeEnum, PipelineStageEnum.Name.ADF_TRANSFORM_VARO, log, null, errorCode, errorMessage, loggerTypeEnum, PipelineFailureReasonEnum.Name.UNKNOWN_FILE_PACKAGE);
                     //string errorMessage = $"Unknown file package";
                     log.LogError($"- {payload.FileName} - {errorMessage}");
                     var result = new ObjectResult(new { statusCode = 500, currentDate = DateTime.Now, message = errorMessage });
@@ -143,7 +143,7 @@ namespace fa_adf_transform_varo
 
 				string errorMessage = EdiErrorsService.BuildExceptionMessageString(e, errorCode, EdiErrorsService.BuildErrorVariableArrayList(payload.FileName));
                 string innerErrorCode = EdiErrorsService.GetInnerErrorCodeFromMessage(errorMessage, errorCode);
-                DataTransformService.LogEmsTransformErrorEventToAppInsights(payload?.FileName, PipelineStageEnum.Name.ADF_TRANSFORM_VARO, log, e, innerErrorCode, errorMessage, loggerTypeEnum, PipelineFailureReasonEnum.Name.UNKNOWN_EXCEPTION);
+                DataTransformService.LogEmsTransformErrorEventToAppInsights(payload?.FileName, emdTypeEnum, PipelineStageEnum.Name.ADF_TRANSFORM_VARO, log, e, innerErrorCode, errorMessage, loggerTypeEnum, PipelineFailureReasonEnum.Name.UNKNOWN_EXCEPTION);
                 if (e is BadRequestException)
                 {
                     string errStr = $"Bad request thrown while validating '{emdType}' transformation request";
--- lib-edi/Models/Domain/Azure/AppInsights/PipelineEvent.cs
+++ lib-edi/Models/Domain/Azure/AppInsights/PipelineEvent.cs
@@ -13,6 +13,8 @@ namespace lib_edi.Models.Azure.AppInsights
 		public DataLoggerTypeEnum.Name LoggerType { get; set; }
 		public PipelineFailureTypeEnum.Name PipelineFailureType { get; set; }
 		public PipelineFailureReasonEnum.Name PipelineFailureReason { get; set; }
+		// NHGH-3057 1652 Add EMD type to app insights logging
+		public EmdEnum.Name EmdType { get; set; }
 		public string ReportFileName { get; set; }
 		public string ErrorMessage { get; set; }
 		public string ErrorCode { get; set; }
